9046c6b51f5d4716c054553b95fe0f15f9be989f
Sergio Rodriguez <SergioR133@icloud.com>
2022-03-03 18:48:13 -0500

    Setting up a simple logger middleware

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 var express = require('express');
 var app = express();
+
 
 console.log("Hello World")
 
@@ -8,11 +10,21 @@ console.log("Hello World")
 // })
 app.use("/public", express.static(__dirname + "/public"))
 
+app.use((req, res, next) => {
+    console.log(req.method, req.path - req.ip);
+    next()
+  })
+  
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 })
 
 app.get('/json', (req, res) => {
+    if(process.env.MESSAGE_STYLE === 'uppercase'){
+        res.json({
+            message: "HELLO JSON"
+        })
+    }
     res.json({
         message: "Hello json"
     })
