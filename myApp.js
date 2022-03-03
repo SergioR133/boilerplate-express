587c9cbf98da4c33dea587c478cc2eb1e239537a
Sergio Rodriguez <SergioR133@icloud.com>
2022-03-03 17:38:28 -0500

    Added some middleware to serve up the public folder

--- myApp.js
+++ myApp.js
@@ -6,6 +6,7 @@ console.log("Hello World")
 // app.get('/', (req, res) => {
 //     res.send("Hello Express")
 // })
+app.use(express.static(__dirname + "/public"))
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
